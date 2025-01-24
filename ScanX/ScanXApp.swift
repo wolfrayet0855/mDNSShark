252a51fc234bc73401dafd63e5c93b2372dd6c77
wolfayet0855 <online_alt02@yahoo.com>
2025-01-24 16:27:53 -0500

    Initial Commit

---
+++ ScanX/ScanXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanXApp.swift
+//  ScanX
+//
+//  Created by user on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ScanXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
