e3d8cfc898173e4197d9ee24b329e3790a3a9082
wolfayet0855 <online_alt02@yahoo.com>
2025-02-01 20:38:41 -0500

    integrating DNSResolve

---
+++ ScanX-Bridging-Header.m
@@ -0,0 +1,8 @@
+//
+//  ScanX-Bridging-Header.m
+//  ScanX
+//
+//  Created by user on 2/1/25.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ScanXApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct ScanXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
